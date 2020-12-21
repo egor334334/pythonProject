1626262ea37e1b8b4931eb4aa49b6b1916eed0a0
Admin <egor334@gmail.com>
2020-12-21 19:42:22 +0300

    add files to project

--- chart/models.py
+++ chart/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 class Musician(models.Model):
-    author = models.CharField(max_length=50)
-    song = models.CharField(max_length=50)
+    author = models.CharField(max_length=50, default='', blank=True, null=True)
+    song = models.CharField(max_length=50, default='', blank=True, null=True)
     position = models.PositiveBigIntegerField(unique=True)
+    def __str__(self):
+        return str(self.author)
 
